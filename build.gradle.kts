fcbb5f63ef4aaa57982539b960104f6167eba40e
buthed010203 <naguiar010203@gmail.com>
2022-11-01 23:12:19 -0400

    Fix jar not including dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
     java
 }
 
-sourceSets["main"].kotlin.srcDirs("src")
-sourceSets["main"].java.srcDirs("src")
+sourceSets["main"].kotlin.srcDir("src")
+sourceSets["main"].java.srcDir("src")
 
 java {
     targetCompatibility = JavaVersion.VERSION_1_8
@@ -21,4 +21,12 @@ dependencies {
     compileOnly("com.github.anuken.mindustryjitpack:core:v140")
 
     annotationProcessor("com.github.Anuken:jabel:0.9.0")
+}
+
+tasks.jar {
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory()) it else zipTree(it) })
+    from(rootDir){
+        include("plugin.hjson")
+    }
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
